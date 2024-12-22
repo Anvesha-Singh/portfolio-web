fb791273ec48f940098ceab27cfd67b2b82d2f20
Anvesha-Singh <anvesha.singh2022@vitstudent.ac.in>
2024-12-22 17:46:36 +0530

    Some changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hallo, I am Anvesha
+              Hyllo, I am Anvesha
             </span>
           </h1>
           <p className="text-orange-500 text-base sm:text-lg mb-6 lg:text-xl">
